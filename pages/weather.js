b4933449ad064ccf49d9c6f30c5ead1a6dd681eb
Yudai Shiratori <toritorikebabu1023x@gmail.com>
2021-01-26 01:44:10 +0900

    add redux

--- pages/weather.js
+++ pages/weather.js
@@ -1,8 +1,14 @@
 import Head from "next/head";
+import { connect } from "react-redux";
+import { useState } from "react";
+import { setInfo } from "../redux/actions/main";
 import Header from "./components/Header";
 import style from "../style/index.less";
 
-export default function Weather() {
+function Weather(props) {
+  const { userInfo, setInfo } = props;
+  const [name, setName] = useState("");
+
   return (
     <div className="container">
       <Head>
@@ -12,10 +18,26 @@ export default function Weather() {
 
       <main>
         <div className={style.main}>
-          <h1 className="title">Today is Looking Good</h1>
+          <h1 className="title">Today is Looking Good for {userInfo.name}</h1>
           <Header />
+          <input
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button onClick={() => setInfo(name)}>Submit</button>
         </div>
       </main>
     </div>
   );
 }
+
+const mapStateToProps = (state) => ({
+  userInfo: state.main,
+});
+
+const mapDispatchToProps = {
+  setInfo: setInfo,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Weather);
